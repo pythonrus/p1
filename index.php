e2d3b1a0c7d5324cd62cb0a7c6cc59d6757509c1
tulou <mistersupersammy@gmail.com>
2016-02-10 16:08:13 -0600

    Modified index.php file

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
-<h1>Sam Chao</h1>
+<!doctype html>
+<html lang="en">
+<html>
+<head>
 
-<h2>Dynamic Web Applications P1</h2>
+  <title>DWA P1</title>
+  <meta charset="utf-8">
 
-<img src='images/costarica.jpg'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style1.css">
+  <link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+
+</head>
+
+<body>
+
+  <div class="container">
+
+    <h1><strong>Sam Chao</strong></h1>
+
+    <h2><b>P1: System Setup / Portfolio<b></h2>
+
+    <img src='images/costarica.jpg'>
+
+    <h4>Corcovado National Park, Costa Rica</h4>
+
+    <h3><b>P2: PHP Basics<b></h3>
+
+    <p>View: [<a href="#">link</a>]</p>
+    <p>Github: [<a href="#">link</a>]</p>
+
+    <h3><b>P3: Laravel Basics<b></h3>
+
+    <p>View: [<a href="#">link</a>]</p>
+    <p>Github: [<a href="#">link</a>]</p>
+
+    <h3><b>P4: Final<b></h3>
+
+    <p>View: [<a href="#">link</a>]</p>
+    <p>Github: [<a href="#">link</a>]</p>
+
+  </div>
+</body>
+</html>
+
+
